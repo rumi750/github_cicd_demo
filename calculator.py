278baa28a97c1711b33b3c239134546b430fbe9f
rumi750 <romeshaali470@gmail.com>
2026-08-26 13:50:38 +0500

    Add docstring to subtract function
    Updated the subtract function to include a docstring.

--- calculator.py
+++ calculator.py
@@ -1,11 +1,8 @@
 def add(a, b):
+    """Returns the sum of two numbers."""
     return a + b
 
+
 def subtract(a, b):
-    return a - b 
-
-def test_add():
-    assert add(5, 2) == 7
-
-def test_subtract():
-    assert subtract(5, 2) == 3
+    """Returns the difference of two numbers."""
+    return a - b
