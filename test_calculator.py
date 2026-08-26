47b5af6c6284b86fa8c70a036d62520368be69b8
rumi750 <romeshaali470@gmail.com>
2026-08-26 13:41:47 +0500

    Refactor add and subtract functions in tests

--- test_calculator.py
+++ test_calculator.py
@@ -1,7 +1,14 @@
-from calculator import add, subtract
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b  # Make sure this is minus (-), NOT plus (+)
+
 
 def test_add():
     assert add(5, 2) == 7
+
 
 def test_subtract():
     assert subtract(5, 2) == 3
